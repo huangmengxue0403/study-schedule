c3bcac8a1900661dd12f70e1523e37e773a15774
snow <huangmengxue1025@163.com>
2021-12-07 14:21:16 +0800

    基于注解的定时任务

---
+++ src/main/java/com/snow/schedule/annotation/MySchedule.java
@@ -0,0 +1,17 @@
+package com.snow.schedule.annotation;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author huangmengxue
+ * @description
+ * @date 2021/12/7 14:12
+ */
+@Component
+public class MySchedule {
+    @Scheduled(cron = "0/5 * * * * ?")
+    public void test() {
+        System.out.println(System.currentTimeMillis()/1000 + ": hello world");
+    }
+}
